5a92e4dee0e5969f9f53e8629fc44dde98b2397d
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-11 17:28:37 +0000

    Correctly handle wrapping paragraphs

--- src/utility/string/wrapText.ts
+++ src/utility/string/wrapText.ts
@@ -2,6 +2,25 @@ import { cleanString } from "./cleanString";
 
 export function wrapText(text: string, limit: number): string {
   text = cleanString(text);
+  let wrappedText = "";
+
+  const paragraphs = text.split("\n\n");
+
+  for (let j = 0; j < paragraphs.length; j++) {
+    const paragraph = paragraphs[j];
+
+    if (j > 0) {
+      wrappedText += "\n\n";
+    }
+
+    wrappedText += wrapParagraph(paragraph, limit);
+  }
+
+  return wrappedText;
+}
+
+export function wrapParagraph(text: string, limit: number): string {
+  text = cleanString(text);
 
   let wrappedText = "";
   let currentLine = "";
