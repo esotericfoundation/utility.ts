b2db9379013c90ee843451b26e6e3623ff28a0dc
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-11 19:48:50 +0000

    Update `wrapText` method

--- src/utility/string/wrapText.ts
+++ src/utility/string/wrapText.ts
@@ -4,39 +4,43 @@ export function wrapText(text: string, limit: number): string {
   text = cleanString(text);
   let wrappedText = "";
 
-  const paragraphs = text.split(/\n\n/);
-
-  console.log(paragraphs)
-
-  for (let j = 0; j < paragraphs.length; j++) {
-    const paragraph = paragraphs[j];
-
-    if (j !== 0) {
-      wrappedText += "\n\n";
-    }
-
-    wrappedText += wrapParagraph(paragraph, limit).trim();
+  const lines = text.split("\n");
+  for (let l = 0; l < lines.length; l++) {
+    const line = lines[l];
+    wrappedText += wrapLine(line, limit) + "\n";
   }
 
-  return wrappedText + "\n";
+  return wrappedText;
 }
 
-export function wrapParagraph(text: string, limit: number): string {
-  let wrappedText = "";
+export function wrapLine(line: string, limit: number): string {
+  let wrappedLine = "";
+
+  line = line.trimEnd();
+
+  console.log(line)
+
+  if (line.length <= limit) {
+    return line;
+  }
+
+  const indent = line.match(/^\s*/)[0];
+  const trimmedLine = line.trimStart();
+
+  const words = trimmedLine.split(" ");
+
   let currentLine = "";
 
-  const words = text.split(" ");
-
-  for (let i = 0; i < words.length; i++) {
-    const word = words[i];
+  for (let w = 0; w < words.length; w++) {
+    const word = words[w];
 
     currentLine += word + " ";
 
-    if (i === words.length - 1 || currentLine.length + words[i + 1].length > limit) {
-      wrappedText += currentLine.trim() + "\n";
+    if (w == words.length - 1 || currentLine.length + words[w + 1].length > limit) {
+      wrappedLine += indent + currentLine.trimEnd() + "\n";
       currentLine = "";
     }
   }
 
-  return wrappedText;
+  return wrappedLine;
 }
