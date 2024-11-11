ccd94be407a2a91d448e83f3e1f934799bee95b1
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-11 17:54:52 +0000

    Format code

--- src/utility/string/wrapText.ts
+++ src/utility/string/wrapText.ts
@@ -30,10 +30,7 @@ export function wrapParagraph(text: string, limit: number): string {
 
     currentLine += word + " ";
 
-    if (
-      i === words.length - 1 ||
-      currentLine.length + words[i + 1].length > limit
-    ) {
+    if (i === words.length - 1 || currentLine.length + words[i + 1].length > limit) {
       wrappedText += currentLine.trim() + "\n";
       currentLine = "";
     }
