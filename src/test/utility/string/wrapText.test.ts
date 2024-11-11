e8bec41d289cdedc1b800d3ca6bb4f94094df8aa
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-11 17:38:14 +0000

    Add tests for text with newlines at the end

--- src/test/utility/string/wrapText.test.ts
+++ src/test/utility/string/wrapText.test.ts
@@ -27,4 +27,31 @@ describe("wrapText", () => {
 
         expect(wrappedText).toBe("This is a\ntest\nstring\n\nwith a\nnewline\ncharacter.\n");
     });
+
+    it("wraps text (with a newline at the end) at the given limit", () => {
+        const text = "This is a test string.\n";
+        const limit = 10;
+
+        const wrappedText = wrapText(text, limit);
+
+        expect(wrappedText).toBe("This is a\ntest\nstring.\n");
+    });
+
+    it("ignores \\n characters (with a newline at the end)", () => {
+        const text = "This is a test string\nwith a newline character.\n";
+        const limit = 10;
+
+        const wrappedText = wrapText(text, limit);
+
+        expect(wrappedText).toBe("This is a\ntest\nstring\nwith a\nnewline\ncharacter.\n");
+    })
+
+    it("respects double \\n characters (with a newline at the end)", () => {
+        const text = "This is a test string\n\nwith a newline character.\n";
+        const limit = 10;
+
+        const wrappedText = wrapText(text, limit);
+
+        expect(wrappedText).toBe("This is a\ntest\nstring\n\nwith a\nnewline\ncharacter.\n");
+    });
 });
