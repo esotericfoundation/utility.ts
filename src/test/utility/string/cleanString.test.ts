e7eca4325d41334e3592da466d75523171c38001
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-11 22:29:46 +0000

    Add `cleanString` test

--- src/test/utility/string/cleanString.test.ts
+++ src/test/utility/string/cleanString.test.ts
@@ -1,6 +1,14 @@
 import { cleanString } from "../../../utility/string/cleanString";
 
 describe("cleanString", () => {
+  it("removes windows-style newline characters", () => {
+    const text = "This is a test string\r\nwith a newline character.";
+
+    const cleanedText = cleanString(text);
+
+    expect(cleanedText).toBe("This is a test string\nwith a newline character.");
+  });
+
   it("ignores double \\n characters", () => {
     const text = "This is a test string\n\nwith a newline character.";
 
