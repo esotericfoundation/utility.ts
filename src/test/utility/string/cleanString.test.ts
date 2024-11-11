a3c709253ecc6183acab6009fe22997f7d1953d7
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-11 20:44:04 +0000

    Do not remove singular newline characters

--- src/test/utility/string/cleanString.test.ts
+++ src/test/utility/string/cleanString.test.ts
@@ -1,14 +1,6 @@
 import { cleanString } from "../../../utility/string/cleanString";
 
 describe("cleanString", () => {
-  it("replaces \\n with a space", () => {
-    const text = "This is a test string\nwith a newline character.";
-
-    const cleanedText = cleanString(text);
-
-    expect(cleanedText).toBe("This is a test string with a newline character.");
-  });
-
   it("ignores double \\n characters", () => {
     const text = "This is a test string\n\nwith a newline character.";
 
--- src/utility/string/cleanString.ts
+++ src/utility/string/cleanString.ts
@@ -1,5 +1,4 @@
 export function cleanString(text: string): string {
   return text
           .replaceAll("\r\n", "\n")
-          .replaceAll(/(?<!\n)\n(?!\n)/g, " ");
 }
