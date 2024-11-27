601bf0de360a98298e28f8f03945ae57385dbd29
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-27 18:53:29 +0000

    Add more documentation

--- src/utility/map/invertMap.ts
+++ src/utility/map/invertMap.ts
@@ -1,3 +1,11 @@
+/**
+ * This method inverts a map, changing all keys to values and all values to keys.
+ * @param map The map to invert.
+ * @returns The inverted map.
+ * @author Esoteric Enderman <esotericenderman@gmail.com>
+ * @copyright 2024 [Esoteric Foundation](https://esoteric.foundation)
+ * @license [GPL-3.0-only](https://github.com/EsotericFoundation/utility.ts/blob/main/LICENSE)
+ */
 export function invertMap<K, V>(map: Map<K, V>): Map<V, K> {
   const invertedMap = new Map<V, K>();
 
--- src/utility/object/objectIncludesValue.ts
+++ src/utility/object/objectIncludesValue.ts
@@ -1,3 +1,12 @@
+/**
+ * This method checks if an object includes a value.
+ * @param object The object to check.
+ * @param value The value to check for.
+ * @returns `true` if the object includes the value, `false` otherwise.
+ * @author Esoteric Enderman <esotericenderman@gmail.com>
+ * @copyright 2024 [Esoteric Foundation](https://esoteric.foundation)
+ * @license [GPL-3.0-only](https://github.com/EsotericFoundation/utility.ts/blob/main/LICENSE)
+ */
 export function objectIncludesValue(object: Object, value: any) {
   return Object.values(object).includes(value);
 }
