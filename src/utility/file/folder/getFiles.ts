78723025ca223a77b73bd3c4dcaaa21ac671c05c
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-27 18:57:16 +0000

    Document `getFiles`

--- src/utility/file/folder/getFiles.ts
+++ src/utility/file/folder/getFiles.ts
@@ -1,7 +1,12 @@
 import { readdirSync } from "fs";
 
 /**
- * Gets files from a directory recursively.
+ * Gets files from a {@link directory} recursively.
+ * @param directory The directory to get files from.
+ * @returns The array of names of the files in the {@link directory}.
+ * @author Esoteric Enderman <esotericenderman@gmail.com>
+ * @copyright 2024 [Esoteric Foundation](https://esoteric.foundation)
+ * @license [GPL-3.0-only](https://github.com/EsotericFoundation/utility.ts/blob/main/LICENSE)
  */
 export function getFiles(directory: string) {
   const output = [];
