eeb62024bc2133f8ad6d9d3c3c0bb23d874087f1
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-09 12:38:54 +0000

    Actually export the function

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 export { invertMap } from "./utility/map/invertMap";
 export { objectIncludesValue } from "./utility/object/objectIncludesValue";
 export { wrapText } from "./utility/string/wrapText";
+export { replaceCharacter } from "./utility/string/replaceCharacter";
